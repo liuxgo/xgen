4b1149e8c2e742b4d00d54b1896e750286d4abb3
liuxgo <liu.xoy@qq.com>
2023-11-19 19:27:46 +0800

    🐞 fix: 邮箱正则表达式，支持用户名中带.

--- packages/xgen/utils/reg/index.ts
+++ packages/xgen/utils/reg/index.ts
@@ -1,3 +1,4 @@
-export const reg_email = new RegExp(/^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/)
+// 邮箱正则表达式，支持用户名中带.
+export const reg_email = new RegExp(/^[A-Za-z0-9\u4e00-\u9fa5\.]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/)
 
 export const reg_mobile = new RegExp(/^1[3|4|5|8|9][0-9]\d{4,8}$/)
